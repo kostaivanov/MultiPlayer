b4f153760e50e942b8cf5f53c07341be79b5e15b
Kosta Ivanov <kostasvetlozarov@gmail.com>
2023-01-06 01:07:22 +0200

    Update PlayerTeleport.cs, PlayerTeleport.cs.meta, and 5 more files...

---
+++ SomeGame/Assets/Teleporter.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+internal class Teleporter : MonoBehaviour
+{
+    [SerializeField] private Transform destination;
+
+    internal Transform GetDestination()
+    {
+        return destination;
+    }
+}
